ca11d5c06aad96039781d2751dd6cfaf1828d0e1
shshsheep <112836912+shshsheep@users.noreply.github.com>
2023-12-13 19:51:51 +0800

    Feature Template Matching as SAD
    Feature Template Matching as SAD

---
+++ 20231213/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "載入圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(393, 345);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(530, 401);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "計算深度";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(530, 361);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "載入圖片";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 392);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(235, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "公分";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(24, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "相機移動距離";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(630, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 18;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Location = new System.Drawing.Point(398, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(402, 345);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(171, 368);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "公分";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(681, 368);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "公分";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(681, 406);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "公分";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(281, 266);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(111, 89);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 25;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(398, 266);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(111, 89);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 26;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(309, 371);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "公分";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(409, 392);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "公分";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ 20231213/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Point matchPoint1;  // 存儲 button1 的匹配座標
+        private Point matchPoint2;  // 存儲 button4 的匹配座標
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "圖像文件(JPeg,GIF,Bump,etc)|*.jpg;*jpeg;*.gif;*.bump;*.tif;*tiff;*.png|所有文件(*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap mybitmap = new Bitmap(openFileDialog.FileName);
+
+                    this.pictureBox1.Image = mybitmap;
+
+                    // 找到紅點座標
+                    Point redPoint1 = FindRedPointCenter(mybitmap, Color.FromArgb(255, 0, 0));
+
+                    // 切出紅點附近小塊圖像
+                    Rectangle templateRect1 = new Rectangle(redPoint1.X - 20, redPoint1.Y - 20, 40, 40); // 設定切割範圍
+                    Bitmap templateImage1 = CropImage(mybitmap, templateRect1);
+
+                    // 顯示座標
+                    label5.Text = $"紅點座標: ({redPoint1.X}, {redPoint1.Y})";
+
+                    // 顯示切割後的模板圖像
+                    pictureBox3.Image = templateImage1;
+
+                    // 轉換模板圖像為 Color 類型的二維數組
+                    Color[][] templateArray = ConvertBitmapToColorArray(templateImage1);
+
+                    // 轉換主圖像為 Color 類型的二維數組
+                    Color[][] mainImageArray = ConvertBitmapToColorArray(mybitmap);
+
+                    // 計算特徵模板匹配
+                    matchPoint1 = CalculateTemplateMatchingSAD(mainImageArray, templateArray);
+
+
+                    // 顯示匹配座標
+                    label3.Text = $"匹配座標: ({matchPoint1.X}, {matchPoint1.Y})";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+        }
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "圖像文件(JPeg,GIF,Bump,etc)|*.jpg;*jpeg;*.gif;*.bump;*.tif;*tiff;*.png|所有文件(*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap mybitmap = new Bitmap(openFileDialog.FileName);
+
+                    this.pictureBox2.Image = mybitmap;
+
+                    // 找到紅點座標
+                    Point redPoint2 = FindRedPointCenter(mybitmap, Color.FromArgb(255, 0, 0));
+
+                    // 切出紅點附近小塊圖像
+                    Rectangle templateRect2 = new Rectangle(redPoint2.X - 20, redPoint2.Y - 20, 40, 40); // 設定切割範圍
+                    Bitmap templateImage2 = CropImage(mybitmap, templateRect2);
+
+                    // 顯示座標
+                    label6.Text = $"紅點座標: ({redPoint2.X}, {redPoint2.Y})";
+
+                    // 顯示切割後的模板圖像
+                    pictureBox4.Image = templateImage2;
+
+                    // 轉換模板圖像為 Color 類型的二維數組
+                    Color[][] templateArray = ConvertBitmapToColorArray(templateImage2);
+
+                    // 轉換主圖像為 Color 類型的二維數組
+                    Color[][] mainImageArray = ConvertBitmapToColorArray(mybitmap);
+
+                    // 計算特徵模板匹配
+                    matchPoint2 = CalculateTemplateMatchingSAD(mainImageArray, templateArray);
+
+
+                    // 顯示匹配座標
+                    label8.Text = $"匹配座標: ({matchPoint2.X}, {matchPoint2.Y})";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+        }
+
+        private Point FindRedPointCenter(Bitmap bitmap, Color targetColor, int tolerance = 80)
+        {
+            int totalX = 0;
+            int totalY = 0;
+            int count = 0;
+
+            // 找到目標顏色的座標
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+
+                    // 比較紅點顏色是否在容錯範圍內
+                    if (Math.Abs(pixelColor.R - targetColor.R) <= tolerance &&
+                        Math.Abs(pixelColor.G - targetColor.G) <= tolerance &&
+                        Math.Abs(pixelColor.B - targetColor.B) <= tolerance)
+                    {
+                        totalX += x;
+                        totalY += y;
+                        count++;
+                    }
+                }
+            }
+
+            if (count > 0)
+            {
+                // 計算中心座標
+                int centerX = totalX / count;
+                int centerY = totalY / count;
+                return new Point(centerX, centerY);
+            }
+
+            // 如果沒有找到目標顏色，可以返回一個特定的值或引發異常
+            throw new Exception($"未找到目標顏色 {targetColor}");
+        }
+
+        private Bitmap CropImage(Bitmap source, Rectangle cropRect)
+        {
+            // 切割圖像
+            Bitmap croppedImage = source.Clone(cropRect, source.PixelFormat);
+            return croppedImage;
+        }
+
+        private Color[][] ConvertBitmapToColorArray(Bitmap bitmap)
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+
+            Color[][] colorArray = new Color[height][];
+
+            for (int y = 0; y < height; y++)
+            {
+                colorArray[y] = new Color[width];
+
+                for (int x = 0; x < width; x++)
+                {
+                    colorArray[y][x] = bitmap.GetPixel(x, y);
+                }
+            }
+
+            return colorArray;
+        }
+
+        private Point CalculateTemplateMatchingSAD(Color[][] mainImage, Color[][] template)
+        {
+            int minSAD = int.MaxValue;
+            Point bestPosition = Point.Empty;
+
+            int S_rows = mainImage.Length;
+            int S_cols = mainImage[0].Length;
+            int T_rows = template.Length;
+            int T_cols = template[0].Length;
+
+            for (int x = 0; x <= S_cols - T_cols; x++)
+            {
+                for (int y = 0; y <= S_rows - T_rows; y++)
+                {
+                    int SAD = 0;
+
+                    for (int j = 0; j < T_cols; j++)
+                    {
+                        for (int i = 0; i < T_rows; i++)
+                        {
+                            Color p_SearchIMG = mainImage[y + i][x + j];
+                            Color p_TemplateIMG = template[i][j];
+
+                            // 計算每個色道的 SAD，但保持在 0 到 255 之間
+                            SAD += Math.Max(0, Math.Min(255, Math.Abs(p_SearchIMG.R - p_TemplateIMG.R)));
+                            SAD += Math.Max(0, Math.Min(255, Math.Abs(p_SearchIMG.G - p_TemplateIMG.G)));
+                            SAD += Math.Max(0, Math.Min(255, Math.Abs(p_SearchIMG.B - p_TemplateIMG.B)));
+                        }
+                    }
+
+                    if (minSAD > SAD)
+                    {
+                        minSAD = SAD;
+
+                        bestPosition = new Point(x, y);
+                    }
+                }
+            }
+
+            return bestPosition;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                // 檢查是否已選擇兩張圖片
+                if (pictureBox1.Image == null || pictureBox2.Image == null)
+                {
+                    MessageBox.Show("請先選擇兩張圖片。");
+                    return;
+                }
+                
+                // 從 button1 獲得匹配座標
+                //Point matchPoint1 = ParsePoint(label3.Text);
+
+                // 從 button4 獲得匹配座標
+                //Point matchPoint2 = ParsePoint(label8.Text);
+
+                // 從使用者輸入中讀取已知的基線值
+                if (!double.TryParse(textBox1.Text, out double knownBaseline))
+                {
+                    MessageBox.Show("請輸入有效的基線值。");
+                    return;
+                }
+
+                // 已知的相機參數
+                double focalLength = 12.07; // 單位：mm
+                double pixelSize = 0.0033450704225352; // 像素大小，單位：mm 每像素
+
+                // 計算視差（兩像素之間的距離）
+                double disparity = Math.Abs(matchPoint2.X - matchPoint1.X) * pixelSize;
+
+                // 計算深度（結果單位：mm）
+                double depth = (focalLength * knownBaseline) / disparity;
+
+                // 將深度值四捨五入到兩位小數
+                depth = Math.Round(depth, 2);
+
+                // 以公分顯示深度
+                label7.Text = $"深度: {depth} 公分";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+        }
+
+        /*
+        private Point ParsePoint(string text)
+        {
+            try
+            {
+                text = text.Trim('(', ')');
+                string[] coordinates = text.Split(',');
+
+                Console.WriteLine($"Debug: Coordinates - {coordinates[0]}, {coordinates[1]}");
+
+                if (coordinates.Length == 2 &&
+                    int.TryParse(coordinates[0].Trim(), out int x) &&
+                    int.TryParse(coordinates[1].Trim(), out int y))
+                {
+                    return new Point(x, y);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Debug: Error - {ex.Message}");
+            }
+
+            throw new FormatException("座標格式錯誤。");
+        }
+        */
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
+/*
+minSAD = VALUE_MAX;
+
+for (size_t x = 0; x <= S_cols - T_cols; x++)
+{
+    for(size_t y = 0; y <= S_rows - T_rows; y++)
+    {
+        SAD = 0.0;
+
+        for (size_t j = 0; j < T_cols; j++)
+            for( size_t i = 0; i < T_rows; i++)
+            {
+                pixel p_SearchIMG = S[y + i][x + j];
+                pixel p_TemplateIMG = T[i][j];
+
+                SAD += abs(p_SearchIMG.Grey - p_TemplateIMG.Grey);
+            }
+
+        if(minSAD > SAD)
+        {
+            minSAD = SAD;
+
+            position.bestRow = y;
+            position.bestCol = x;
+            position.bestSAD = SAD;
+        }
+    }
+}
+*/
