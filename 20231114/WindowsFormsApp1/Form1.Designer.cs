f2b3cb95abcd835faa6d59f6d885295708a5ddbf
shshsheep <112836912+shshsheep@users.noreply.github.com>
2023-12-26 11:48:34 +0800

    High Boost filtering
    High Boost filtering

---
+++ 20231114/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "載入圖片檔並灰階化";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 223);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(274, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(257, 223);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(334, 343);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "高增幅濾波(A+4)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(491, 344);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(518, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "A的值";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(625, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "高增幅濾波(A+8)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(537, 62);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(257, 223);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ 20231114/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "圖像文件(JPeg,GIF,Bump,etc)|*.jpg;*jpeg;*.gif;*.bump;*.tif;*tiff;*.png|所有文件(*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap mybitmap = new Bitmap(openFileDialog.FileName);
+                    Color pixel;
+                    for (int x = 0; x < mybitmap.Width; x++)
+                        for (int y = 0; y < mybitmap.Height; y++)
+                        {
+                            pixel = mybitmap.GetPixel(x, y);
+                            int r, g, b, result = 0;
+                            r = pixel.R;
+                            g = pixel.G;
+                            b = pixel.B;
+                            result = (299 * r + 587 * g + 114 * b) / 1000;
+                            mybitmap.SetPixel(x, y, Color.FromArgb(result, result, result));
+                        }
+                    this.pictureBox1.Image = mybitmap;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int height = this.pictureBox1.Image.Height;
+                int width = this.pictureBox1.Image.Width;
+                Bitmap newbitmap = new Bitmap(width, height);
+                Bitmap oldbitmap = (Bitmap)this.pictureBox1.Image;
+
+                double A = 0;
+
+                double[,] sobelX = new double[,] { { 0, -1, 0 }, { -1, A + 4, -1 }, { 0, -1, 0 } };
+
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    if (double.TryParse(textBox1.Text, out A))
+                    {
+                        sobelX[1, 1] = A + 4;
+                    }
+                    else
+                    {
+                        // 處理無法解析為 double 的情況，可以提示用戶或採取其他適當的措施。
+                        MessageBox.Show("請輸入有效的數字", "錯誤");
+                        return;
+                    }
+                }
+
+                double maxGradient = 0;
+                double minGradient = double.MaxValue;
+
+                for (int x = 1; x < width - 1; x++)
+                {
+                    for (int y = 1; y < height - 1; y++)
+                    {
+                        double sum = 0;
+
+                        // 使用新的濾波器
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                sum += oldbitmap.GetPixel(x + i, y + j).R * sobelX[i + 1, j + 1];
+                            }
+                        }
+
+                        double gradient = Math.Abs(sum);
+
+                        minGradient = Math.Min(minGradient, gradient);
+                        maxGradient = Math.Max(maxGradient, gradient);
+                    }
+                }
+
+                double scaleFactor = 255.0 / (maxGradient - minGradient);
+
+                for (int x = 1; x < width - 1; x++)
+                {
+                    for (int y = 1; y < height - 1; y++)
+                    {
+                        double sum = 0;
+
+                        // 使用新的濾波器
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                sum += oldbitmap.GetPixel(x + i, y + j).R * sobelX[i + 1, j + 1];
+                            }
+                        }
+
+                        double gradient = Math.Abs(sum);
+
+                        //gradient = (int)((gradient - minGradient) * scaleFactor);
+
+                        gradient = Math.Max(0, Math.Min(gradient, 255));
+
+                        newbitmap.SetPixel(x, y, Color.FromArgb((int)gradient, (int)gradient, (int)gradient));
+                    }
+                }
+
+                this.pictureBox2.Image = newbitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int height = this.pictureBox1.Image.Height;
+                int width = this.pictureBox1.Image.Width;
+                Bitmap newbitmap = new Bitmap(width, height);
+                Bitmap oldbitmap = (Bitmap)this.pictureBox1.Image;
+
+                double A = 0;
+
+                double[,] sobelX = new double[,] { { -1, -1, -1 }, { -1, A + 8, -1 }, { -1, -1, -1 } };
+
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    if (double.TryParse(textBox1.Text, out A))
+                    {
+                        sobelX[1, 1] = A + 8;
+                    }
+                    else
+                    {
+                        // 處理無法解析為 double 的情況，可以提示用戶或採取其他適當的措施。
+                        MessageBox.Show("請輸入有效的數字", "錯誤");
+                        return;
+                    }
+                }
+
+                double maxGradient = 0;
+                double minGradient = double.MaxValue;
+
+                for (int x = 1; x < width - 1; x++)
+                {
+                    for (int y = 1; y < height - 1; y++)
+                    {
+                        double sum = 0;
+
+                        // 使用新的濾波器
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                sum += oldbitmap.GetPixel(x + i, y + j).R * sobelX[i + 1, j + 1];
+                            }
+                        }
+
+                        double gradient = Math.Abs(sum);
+
+                        minGradient = Math.Min(minGradient, gradient);
+                        maxGradient = Math.Max(maxGradient, gradient);
+                    }
+                }
+
+                double scaleFactor = 255.0 / (maxGradient - minGradient);
+
+                for (int x = 1; x < width - 1; x++)
+                {
+                    for (int y = 1; y < height - 1; y++)
+                    {
+                        double sum = 0;
+
+                        // 使用新的濾波器
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                sum += oldbitmap.GetPixel(x + i, y + j).R * sobelX[i + 1, j + 1];
+                            }
+                        }
+
+                        double gradient = Math.Abs(sum);
+
+                        //gradient = (int)((gradient - minGradient) * scaleFactor);
+
+                        gradient = Math.Max(0, Math.Min(gradient, 255));
+
+                        newbitmap.SetPixel(x, y, Color.FromArgb((int)gradient, (int)gradient, (int)gradient));
+                    }
+                }
+
+                this.pictureBox3.Image = newbitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+        }
+
+        
+    }
+}
